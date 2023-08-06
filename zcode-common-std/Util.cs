3bf0d4dfed3bccbca69767f7be813be2a423f9a1
Charles Montgomery <charles.montgomery@charter.net>
2023-08-05 23:12:50 -0500

    Utility Functions

---
+++ zcode-common-std/Util.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace zcode_common_std
+{
+	public class Util
+	{
+		public Util()
+		{
+		}
+		public class LazyVariable<T> where T : class
+		{
+			private Func<T> _creator;
+			private T value;
+			private LazyVariable()
+			{
+			}
+			public static LazyVariable<T> Create(Func<T> creator)
+			{
+				var outx = new LazyVariable<T>();
+				outx._creator = creator;
+				return outx;
+			}
+			public T Get => ((Func<T>)(() =>
+			{
+				return value = value != null ? value : _creator();
+			}))();
+		}
+	}
+}
+
